3f504e18db8f01527909f0317d384d7511ac2507
watt <watt@micous.com>
2025-06-11 12:34:43 +0800

    add multi file

--- coreemail.go
+++ coreemail.go
@@ -5,53 +5,62 @@ import (
 	"mime"
 	"path/filepath"
 
+	"github.com/go-playground/validator/v10"
 	"gopkg.in/gomail.v2"
 )
 
 type Mail struct {
-	SenderAddr   string   // 发件人地址
-	SenderName   string   // 发件人名称
-	ReceiverAddr []string // 收件人地址，可以有多个收件人
-	Subject      string   // 邮件主题
+	SenderAddr   string   `validate:"required,email"`         // 必填且符合邮箱格式
+	SenderName   string   `validate:"required,min=1,max=20"`  // 必填，长度1-50字符
+	ReceiverAddr []string `validate:"required,dive,email"`    // 必填，每个元素需为邮箱格式
+	Subject      string   `validate:"required,min=1,max=100"` // 必填，长度1-100字符
 	Text         string   // 正文
-	FilePath     string   //附件地址
-	Host         string   // 邮件服务器地址
-	Port         int      // 邮件服务器端口号
-	Username     string   // 用户名
-	Password     string   // 密码或授权码
+	FilePaths    []string `validate:"dive,file"`                // 每个附件路径需为有效文件路径[5,7](@ref)
+	Host         string   `validate:"required,hostname|ipv4"`   // 必填，需为域名或IPv4地址[1,7](@ref)
+	Port         int      `validate:"required,min=1,max=65535"` // 必填，端口号范围1-65535
+	Username     string   `validate:"required"`                 // 必填
+	Password     string   `validate:"required"`                 // 必填
 }
 
-// SendMail(&Mail{
-// 	SenderAddr:   "sender@163.com",
-// 	SenderName:   "senderName",
-// 	ReceiverAddr: []string{"receiver@163.com", "receiver@163.com"},
-// 	Subject:      "subject",
-// 	Text:         "test",
-// 	FilePath:	  "C:\title.txt"
-// 	Host:         "smtp.163.com",
-// 	Port:         25,
-// 	Username:     "username@163.com",
-// 	Password:     "password",
-// })
-func SendMail(s *Mail) error {
+func (mail *Mail) Validate() error {
+	validate := validator.New()
+	err := validate.Struct(mail)
+	return err
+}
+
+//	(&Mail{
+//		SenderAddr:   "sender@163.com",
+//		SenderName:   "senderName",
+//		ReceiverAddr: []string{"receiver@163.com", "receiver@163.com"},
+//		Subject:      "subject",
+//		Text:         "test",
+//		FilePaths:	  []string("C:\title.txt")
+//		Host:         "smtp.163.com",
+//		Port:         25,
+//		Username:     "username@163.com",
+//		Password:     "password",
+//	}).Send()
+func (mail *Mail) Send() error {
 	m := gomail.NewMessage()
 	m.SetHeaders(map[string][]string{
-		"From":    {m.FormatAddress(s.SenderAddr, s.SenderName)}, // 发件人邮箱，发件人名称
-		"To":      s.ReceiverAddr,                                // 多个收件人
-		"Subject": {s.Subject},                                   // 邮件主题
+		"From":    {m.FormatAddress(mail.SenderAddr, mail.SenderName)}, // 发件人邮箱，发件人名称
+		"To":      mail.ReceiverAddr,                                   // 多个收件人
+		"Subject": {mail.Subject},                                      // 邮件主题
 	})
-	m.SetBody("text/plain", s.Text)
-	if len(s.FilePath) > 0 {
-		fileName := filepath.Base(s.FilePath)
-		m.Attach(s.FilePath,
-			gomail.Rename(fileName),
-			gomail.SetHeader(map[string][]string{
-				"Content-Disposition": {
-					fmt.Sprintf(`attachment; filename="%s"`, mime.QEncoding.Encode("UTF-8", fileName)),
-				},
-			}),
-		)
+	m.SetBody("text/plain", mail.Text)
+	if len(mail.FilePaths) > 0 {
+		for _, filePath := range mail.FilePaths {
+			fileName := filepath.Base(filePath)
+			m.Attach(filePath,
+				gomail.Rename(fileName),
+				gomail.SetHeader(map[string][]string{
+					"Content-Disposition": {
+						fmt.Sprintf(`attachment; filename="%s"`, mime.QEncoding.Encode("UTF-8", fileName)),
+					},
+				}),
+			)
+		}
 	}
-	d := gomail.NewDialer(s.Host, s.Port, s.Username, s.Password) // 发送邮件服务器、端口号、发件人账号、发件人密码
+	d := gomail.NewDialer(mail.Host, mail.Port, mail.Username, mail.Password) // 发送邮件服务器、端口号、发件人账号、发件人密码
 	return d.DialAndSend(m)
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,19 @@ module github.com/dxasu/coreemail
 
 go 1.18
 
-require gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df // indirect
+require (
+	github.com/go-playground/validator/v10 v10.27.0
+	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
+)
+
+require (
+	github.com/gabriel-vasile/mimetype v1.4.8 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/leodido/go-urn v1.4.0 // indirect
+	golang.org/x/crypto v0.33.0 // indirect
+	golang.org/x/net v0.34.0 // indirect
+	golang.org/x/sys v0.30.0 // indirect
+	golang.org/x/text v0.22.0 // indirect
+	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
+)
